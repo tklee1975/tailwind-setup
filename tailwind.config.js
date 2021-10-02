5b13d688470e80b3ed98846ef4dbb12d804bbc24
kenlee <lakoo.kenlee@gmail.com>
2021-10-02 13:34:50 +0800

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './*.html',
+      './**/*.html'
+    ]
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'content' : ['Open Sans', 'Arial', 'sans-serif'],
+      'display' : ['Josefin Sans', 'Serif'],
+    },
+    extend: {
+      colors: {
+        'highlight' : '#f53',
+        'primary' : '#7fa',
+        'secondry' : '#aaa',
+        'success' : '#494',
+        'error' : '#f00',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
